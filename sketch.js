dd0bcc449229260127b9e3639d668af6c14ff555
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2020-11-19 08:22:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var END = 0;
 var gameState = PLAY;
 
 var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var ground, invisibleGround, groundImage
 
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
